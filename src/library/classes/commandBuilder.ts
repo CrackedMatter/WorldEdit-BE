ad5a38a07dd40872a7518b2c8e0b875b27852f13
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2023-04-27 17:15:23 +0200

    Move invalid flag checking to end of command

--- src/library/classes/commandBuilder.ts
+++ src/library/classes/commandBuilder.ts
@@ -302,6 +302,7 @@ export class CommandBuilder {
 
       let defIdx = 0;
       let hasNamedSubCmd = false;
+      let invalidFlags: string[] = [];
       flagDefs = new Map<string, commandFlag>(flagDefs);
       argDefs?.forEach(argDef => {
         if ("flag" in argDef && !flagDefs.has(argDef.flag)) {
@@ -350,6 +351,13 @@ export class CommandBuilder {
             idx = processList(i, sub.args, subResult, flagDefs);
             result.set(sub.subName, true);
             subResult.forEach((v, k) => result.set(k, v));
+            invalidFlags.forEach((f, i) => {
+              if (sub.args.map(argDef => argDef.flag).includes(f)) {
+                result.set(f, true);
+                invalidFlags[i] = "";
+              }
+            });
+            invalidFlags = invalidFlags.filter(f => f !== "");
             break;
           } catch (e) {
             fails.push(e);
@@ -381,7 +389,7 @@ export class CommandBuilder {
                 }, result);
               }
             } else {
-              throw RawText.translate("commands.generic.wedit:invalidFlag").with(f);
+              invalidFlags.push(f);
             }
           }
           continue;
@@ -434,6 +442,10 @@ export class CommandBuilder {
           }
         }
         defIdx++;
+      }
+
+      if (invalidFlags.length != 0) {
+        throw RawText.translate("commands.generic.wedit:invalidFlag").with(invalidFlags[0]);
       }
 
       return i;
